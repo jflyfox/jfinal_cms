4826539b61900a149f17ec431e6e541044adbf03
FLY的狐狸 <369191470@sina.com>
2015-06-20 17:02:14 +0800

    加入用户发布博文功能~

--- src/com/flyfox/modules/front/PersonController.java
+++ src/com/flyfox/modules/front/PersonController.java
@@ -37,6 +37,123 @@ public class PersonController extends BaseController {
 		SysUser user = getSessionAttr(Attr.SESSION_NAME);
 		if (user == null) {
 			redirect(CommonController.firstPage);
+			return;
+		}
+
+		// 活动目录
+		setAttr("folders_selected", "personhome");
+
+		setAttr("user", user);
+
+		// 数据列表,只查询展示的和类型为11,12的
+		Page<TbArticle> articles = TbArticle.dao.paginate(getPaginator(), "select * ", //
+				" from tb_article where status = 1 and type in (11,12) " //
+						+ " and create_id = ? " //
+						+ " order by sort,create_time desc", user.getUserid());
+		setAttr("page", articles);
+
+		// 显示50个标签
+		if (articles.getTotalRow() > 0) {
+			Page<TbTags> taglist = new FrontCacheService().getTagsByFolder(new Paginator(1, 50), articles.getList()
+					.get(0).getFolderId());
+			setAttr("taglist", taglist.getList());
+		} else {
+			Page<TbTags> taglist = new FrontCacheService().getTags(new Paginator(1, 50));
+			setAttr("taglist", taglist.getList());
+		}
+
+		// title展示
+		setAttr(JFlyFoxUtils.TITLE_ATTR,
+				"空间 - " + BeeltFunctions.getUserName(user.getUserid()) + " - " + JFlyFoxCache.getHeadTitle());
+
+		renderAuto(path + "workspace.html");
+	}
+
+	/**
+	 * 跳转到发布博文页面
+	 * 
+	 * 2015年6月17日 下午9:53:04 flyfox 330627517@qq.com
+	 */
+	@Before(FrontInterceptor.class)
+	public void newblog() {
+		SysUser user = getSessionAttr(Attr.SESSION_NAME);
+		if (user == null) {
+			redirect(CommonController.firstPage);
+			return;
+		}
+
+		setAttr("user", user);
+
+		// 活动目录
+		setAttr("folders_selected", "personhome");
+
+		renderAuto(path + "new_blog.html");
+	}
+
+	/**
+	 * 保存博文
+	 * 
+	 * 2015年6月17日 下午10:12:18 flyfox 330627517@qq.com
+	 */
+	public void saveblog() {
+		JSONObject json = new JSONObject();
+		json.put("status", 2);// 失败
+
+		SysUser user = getSessionAttr(Attr.SESSION_NAME);
+		if (user == null) {
+			json.put("msg", "没有登录，不能提交博文！");
+			renderJson(json.toJSONString());
+			return;
+		}
+
+		TbArticle model = getModel(TbArticle.class);
+		model.remove("id");
+		// TODO 验证题目，内容
+
+		model.setFolderId(100); // 博文目录
+		model.setStatus("1"); // 显示
+		model.setType(11);
+		model.setIsComment(1); // 能评论
+		model.setIsRecommend(2);// 不推荐
+		model.setSort(20); // 排序
+		model.setCreateId(getSessionUser().getUserID());
+		model.setCreateTime(getNow());
+		model.save();
+
+		String tags = getPara("tags");
+		if (StrUtils.isNotEmpty(tags)) {
+			String[] tagsArr = tags.split(",");
+			for (int i = 0; i < tagsArr.length; i++) {
+				String tagname = tagsArr[i];
+				// 最多5个
+				if (i >= 5) {
+					break;
+				}
+				if (StrUtils.isEmpty(tagname)) {
+					continue;
+				}
+				TbTags tbTags = new TbTags();
+				tbTags.put("tagname", tagname);
+				tbTags.put("article_id", model.getInt("id"));
+				tbTags.put("create_id", getSessionUser().getUserID());
+				tbTags.put("create_time", getNow());
+				tbTags.save();
+
+			}
+		}
+
+		json.put("status", 1);// 成功
+		renderJson(json.toJSONString());
+	}
+	
+	/**
+	 * 个人信息
+	 */
+	@Before(FrontInterceptor.class)
+	public void profile() {
+		SysUser user = getSessionAttr(Attr.SESSION_NAME);
+		if (user == null) {
+			redirect(CommonController.firstPage);
 		} else {
 			setAttr("model", user);
 
