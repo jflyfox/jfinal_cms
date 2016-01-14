2eebdfc88af2481bb890a8a1cb415e5133975831
FLY的狐狸 <369191470@sina.com>
2016-01-14 23:33:41 +0800

    加入测试目录

---
+++ test/com/test/jflyfox/TestJflyfox.java
@@ -0,0 +1,14 @@
+package com.test.jflyfox;
+
+/**
+ * 测试类
+ * 
+ * 2016年1月14日 下午11:32:17
+ * flyfox 330627517@qq.com
+ */
+public class TestJflyfox {
+
+	public static void main(String[] args) {
+		System.out.println("hello everybody!!!");
+	}
+}
