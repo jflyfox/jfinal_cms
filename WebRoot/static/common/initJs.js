95d9387de38d1c8b009b9b3e880f7152359e4d34
FLY的狐狸 <369191470@sina.com>
2016-01-16 17:35:56 +0800

    加入排序公共方法

--- WebRoot/static/common/initJs.js
+++ WebRoot/static/common/initJs.js
@@ -8,8 +8,53 @@ jQuery(function($) {
         }
     });
 	
+	mySort();
+	
+	// 分页
 	myInit();
+	
 });
+
+function mySort(){
+	var $orderColumn = $(":input[name='form.orderColumn']")||'';
+	var $orderAsc = $(":input[name='form.orderAsc']")||'';
+	if($orderColumn!=''){
+		$("th.sorting[name='"+$orderColumn.val()+"']").removeClass('sorting').addClass('sorting_'+$orderAsc.val());
+	}
+	
+	$('.sorting,.sorting_asc,.sorting_desc').on('click',function(){
+			var className = $(this).attr('class');
+			if(className=='sorting_asc') {
+				mySortClear();
+				$(this).removeClass('sorting').addClass('sorting_desc');
+				$orderColumn.val($(this).attr('name'));
+				$orderAsc.val('desc');
+			}else if(className=='sorting_desc') {
+				mySortClear();
+				$(this).removeClass('sorting').addClass('sorting_asc');
+				$orderColumn.val($(this).attr('name'));
+				$orderAsc.val('asc');
+			} else {
+				mySortClear();
+				$(this).removeClass('sorting').addClass('sorting_asc');
+				$orderColumn.val();
+				$orderColumn.val($(this).attr('name'));
+				$orderAsc.val('asc');
+			}
+			if(typeof sorting!='undefined'&&sorting instanceof Function) sorting();
+			else if(typeof oper.list!='undefined'&&oper.list instanceof Function) oper.list();
+	});
+}
+
+/**
+ * 还原排序状态
+ */
+function mySortClear(){
+	$('[class^="sorting_"]').each(function(i){
+		var className = $(this).attr('class');
+		$(this).removeClass(className).addClass('sorting');
+	});
+}
 
 function myInit(){
 	var $paginator = $('#paginator');
@@ -32,8 +77,10 @@ function myInit(){
 			next: '>', prev: '<', first: '<<', last: '>>', theme: 'blue', 
 			controlsalways: true, onchange: function (newPage) {
 				$('#paginator_pageno').val(newPage);
-				if(typeof paginator!='undefined'&paginator instanceof Function) paginator(newPage);
+				if(typeof paginator!='undefined'&&paginator instanceof Function) paginator(newPage);
+				else if(typeof oper.list!='undefined'&&oper.list instanceof Function) oper.list();
 	    	}
 	    });
 	}
 }
+
